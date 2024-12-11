062c1586667439ee02d68113051fbd850df1d2b7
dusan-rychnovsky <durychno@microsoft.com>
2024-12-11 09:14:57 +0100

    backend empty spring-boot project

---
+++ src/backend/src/main/java/cz/dusanrychnovsky/myteacollection/MyTeaCollectionApplication.java
@@ -0,0 +1,11 @@
+package cz.dusanrychnovsky.myteacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTeaCollectionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyTeaCollectionApplication.class, args);
+	}
+}
---
+++ src/backend/src/test/java/cz/dusanrychnovsky/myteacollection/MyTeaCollectionApplicationTests.java
@@ -0,0 +1,11 @@
+package cz.dusanrychnovsky.myteacollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTeaCollectionApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
