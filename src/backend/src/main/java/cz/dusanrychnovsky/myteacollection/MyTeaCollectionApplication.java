66a2855124d137ad5abe9ade93604d63822049c4
dusan-rychnovsky <durychno@microsoft.com>
2024-12-17 09:08:05 +0100

    backend hello world route

--- src/backend/src/main/java/cz/dusanrychnovsky/myteacollection/MyTeaCollectionApplication.java
+++ src/backend/src/main/java/cz/dusanrychnovsky/myteacollection/MyTeaCollectionApplication.java
@@ -2,10 +2,23 @@ package cz.dusanrychnovsky.myteacollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// https://dev.to/philipathanasopoulos/guide-to-free-hosting-for-your-full-stack-spring-boot-application-4fak
+// https://spring.io/quickstart
 
 @SpringBootApplication
+@RestController
 public class MyTeaCollectionApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MyTeaCollectionApplication.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyTeaCollectionApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 }
