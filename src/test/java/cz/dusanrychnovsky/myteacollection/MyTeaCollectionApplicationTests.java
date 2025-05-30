ac191bd78a063062ab4d4a2ccfa38b91754e3770
dusan-rychnovsky <durychno@microsoft.com>
2025-05-30 20:02:03 +0200

    Search - integration test.

--- src/test/java/cz/dusanrychnovsky/myteacollection/MyTeaCollectionApplicationTests.java
+++ src/test/java/cz/dusanrychnovsky/myteacollection/MyTeaCollectionApplicationTests.java
@@ -1,7 +1,9 @@
 package cz.dusanrychnovsky.myteacollection;
 
+import cz.dusanrychnovsky.myteacollection.model.SearchCriteria;
 import cz.dusanrychnovsky.myteacollection.util.upload.UploadNewTeas;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,11 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.io.IOException;
 
 import static cz.dusanrychnovsky.myteacollection.util.ClassLoaderUtils.toFile;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.TestInstance.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,83 +30,128 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestInstance(Lifecycle.PER_CLASS)
 class MyTeaCollectionApplicationTests {
 
-	@Autowired
-	private UploadNewTeas uploadNewTeas;
+  @Autowired
+  private UploadNewTeas uploadNewTeas;
 
-	@Autowired
-	private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-	@BeforeAll
-	void setup() throws IOException {
-		// insert two teas in the DB
-		uploadNewTeas.run(toFile("teas"));
-	}
+  @BeforeAll
+  void setup() throws IOException {
+    // insert two teas in the DB
+    uploadNewTeas.run(toFile("teas"));
+  }
 
-	@Test
-	void index_listsAllTeas() throws Exception {
-		// request index action
-		var actions = mvc.perform(get("/index"))
-			.andExpect(status().isOk());
+  @Test
+  void index_listsAllTeas() throws Exception {
+    var actions = mvc.perform(get("/index"))
+      .andExpect(status().isOk());
 
-		// verify tea type dropdown
-		actions=containsStrings(actions,
-			"<option value=\"1\">Blend</option>",
-			"<option value=\"2\">White Tea</option>",
-			"<option value=\"5\">Oolong</option>",
-			"<option value=\"8\">Sheng Puerh</option>",
-			"<option value=\"11\">Yabao</option>",
-			"<option value=\"12\">Fu Zhuan</option>");
+    verifyHeader(actions);
+    verifyDropdowns(actions);
 
-		// verify vendor dropdown
-		actions=containsStrings(actions,
-			"<option value=\"1\">Mei Leaf</option>",
-			"<option value=\"2\">Meetea</option>",
-			"<option value=\"5\">Klasek Tea</option>",
-			"<option value=\"6\">Banna House</option>");
+    verifyTea(
+      actions,
+      "Doubleshot",
+      "Ming Feng Shan Lao Shu Shu Puer Bing Cha 2022",
+      "Meetea",
+      "Dark Tea, Shu Puerh"
+    );
+    verifyTea(
+      actions,
+      "Luminary Misfit",
+      "Lancang Gushu Sheng PuErh Spring 2022",
+      "Mei Leaf",
+      "Dark Tea, Sheng Puerh"
+    );
+  }
 
-		// verify availability dropdown
-		actions=containsStrings(actions,
-			"<option value=\"1\">In stock</option>",
-      "<option value=\"2\">Out of stock</option>");
+  @Test
+  void teaView_showsGivenTea() throws Exception {
+    var actions = mvc.perform(get("/teas/2"))
+      .andExpect(status().isOk());
 
-		// verify list of teas
-		actions = containsStrings(actions,
-			"<h1 class=\"jumbotron-heading\">My Tea Collection</h1>",
-			"<span>Doubleshot</span>",
-			"<span>Ming Feng Shan Lao Shu Shu Puer Bing Cha 2022</span>",
-			"<span>Meetea</span>",
-			"<span>Dark Tea, Shu Puerh</span>",
-			"<span>Luminary Misfit</span>",
-			"<span>Lancang Gushu Sheng PuErh Spring 2022</span>",
-			"<span>Mei Leaf</span>",
-			"<span>Dark Tea, Sheng Puerh</span>");
-	}
+    containsStrings(actions,
+      "<span>Luminary Misfit</span>",
+      "<div class=\"subtitle mt-1\">Lancang Gushu Sheng PuErh Spring 2022</div>",
+      "<li>Dark Tea</li>",
+      "<li>Sheng Puerh</li>",
+      "<span>Mei Leaf</span>",
+      "<a href=\"https://meileaf.com/tea/luminary-misfit/\">meileaf.com</a>",
+      "<span>April 2022</span>",
+      "<span>Lancang, Puer, Yunnan, China</span>",
+      "<span>1740-1970m</span>",
+      "<span>Da Ye Zhong</span>",
+      "<span>95°C, 5g/100ml, 25+5s</span>");
+  }
 
-	@Test
-	void teaView_showsGivenTea() throws Exception {
-		// request teaView action
-		var actions = mvc.perform(get("/teas/2"))
-			.andExpect(status().isOk());
+  @Disabled()
+  @Test()
+  void search_listsRelevantTeas() throws Exception {
+    var actions = mvc.perform(post("/search", new SearchCriteria("shot")))
+      .andExpect(status().isOk());
 
-		// verify tea properties
-		actions=containsStrings(actions,
-			"<span>Luminary Misfit</span>",
-			"<div class=\"subtitle mt-1\">Lancang Gushu Sheng PuErh Spring 2022</div>",
-			"<li>Dark Tea</li>",
-			"<li>Sheng Puerh</li>",
-			"<span>Mei Leaf</span>",
-			"<a href=\"https://meileaf.com/tea/luminary-misfit/\">meileaf.com</a>",
-			"<span>April 2022</span>",
-			"<span>Lancang, Puer, Yunnan, China</span>",
-			"<span>1740-1970m</span>",
-			"<span>Da Ye Zhong</span>",
-			"<span>95°C, 5g/100ml, 25+5s</span>");
-	}
+    verifyHeader(actions);
+    verifyDropdowns(actions);
 
-	private ResultActions containsStrings(ResultActions actions, String... strings) throws Exception {
-		for (var str : strings) {
-			actions = actions.andExpect(content().string(containsString(str)));
-		}
-		return actions;
-	}
+    verifyTea(
+      actions,
+      "Doubleshot",
+      "Ming Feng Shan Lao Shu Shu Puer Bing Cha 2022",
+      "Meetea",
+      "Dark Tea, Shu Puerh"
+    );
+
+    actions.andExpect(
+      content().string(
+        not(
+          containsString("Luminary Misfit")
+        )
+      )
+    );
+  }
+
+  private void verifyHeader(ResultActions actions) throws Exception {
+    containsStrings(actions,"<h1 class=\"jumbotron-heading\">My Tea Collection</h1>");
+  }
+
+  private void verifyDropdowns(ResultActions actions) throws Exception {
+    containsStrings(actions,
+      // verify tea type dropdown
+      "<option value=\"1\">Blend</option>",
+      "<option value=\"2\">White Tea</option>",
+      "<option value=\"5\">Oolong</option>",
+      "<option value=\"8\">Sheng Puerh</option>",
+      "<option value=\"11\">Yabao</option>",
+      "<option value=\"12\">Fu Zhuan</option>",
+      // verify vendor dropdown
+      "<option value=\"1\">Mei Leaf</option>",
+      "<option value=\"2\">Meetea</option>",
+      "<option value=\"5\">Klasek Tea</option>",
+      "<option value=\"6\">Banna House</option>",
+      // verify availability dropdown
+      "<option value=\"1\">In stock</option>",
+      "<option value=\"2\">Out of stock</option>"
+    );
+  }
+
+  private void verifyTea(
+    ResultActions actions, String title, String name, String vendor, String types)
+    throws Exception {
+
+    containsStrings(actions,
+      "<span>" + title + "</span>",
+      "<span>" + name + "</span>",
+      "<span>" + vendor + "</span>",
+      "<span>" + types + "</span>"
+    );
+  }
+
+  private void containsStrings(ResultActions actions, String... strings) throws Exception {
+    for (var str : strings) {
+      actions.andExpect(content().string(
+        containsString(str)
+      ));
+    }
+  }
 }
