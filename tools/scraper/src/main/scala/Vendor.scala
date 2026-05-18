00d7860df1a8021c8d4967c09586b10c803c3823
dusan-rychnovsky <dusan.rychnovsky@gmail.com>
2026-05-18 09:46:30 +0200

    SCRAPER: Refactoring.

--- tools/scraper/src/main/scala/Vendor.scala
+++ tools/scraper/src/main/scala/Vendor.scala
@@ -4,13 +4,16 @@ import zio.http.*
 final class UnsupportedVendor(host: String)
   extends RuntimeException(s"no vendor registered for host: $host")
 
-enum Vendor(val displayName: String, val host: String):
-  case MeiLeaf extends Vendor("Mei Leaf", "meileaf.com")
-  case Meetea  extends Vendor("Meetea", "store.meetea.cz")
+enum Vendor(
+    val displayName: String,
+    val host: String,
+    parse: (String, URL) => IO[ParseError, TeaInfo]
+):
+  case MeiLeaf extends Vendor("Mei Leaf", "meileaf.com", parseMeileafTea)
+  case Meetea  extends Vendor("Meetea", "store.meetea.cz", parseMeeteaTea)
 
-  def scrape(url: URL): ZIO[Client, Throwable, TeaInfo] = this match
-    case Vendor.MeiLeaf => fetch(url.encode).flatMap(parseMeileafTea(_, url))
-    case Vendor.Meetea  => fetch(url.encode).flatMap(parseMeeteaTea(_, url))
+  def scrape(url: URL): ZIO[Client, Throwable, TeaInfo] =
+    fetch(url.encode).flatMap(parse(_, url))
 
 def selectVendor(url: URL): IO[UnsupportedVendor, Vendor] =
   ZIO
