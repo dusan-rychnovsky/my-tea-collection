76537fc452fa948e781a39b656e559662575cd38
dusan-rychnovsky <dusan.rychnovsky@gmail.com>
2026-05-25 09:26:25 +0200

    SCRAPER: Refactoring.

--- tools/scraper/src/main/scala/cz/dusanrychnovsky/myteacollection/scraper/parser/Meileaf.scala
+++ tools/scraper/src/main/scala/cz/dusanrychnovsky/myteacollection/scraper/parser/Meileaf.scala
@@ -1,8 +1,9 @@
 package cz.dusanrychnovsky.myteacollection.scraper.parser
 
 import cz.dusanrychnovsky.myteacollection.scraper.domain.*
-
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
 import scala.jdk.CollectionConverters.*
 import zio.*
 import zio.http.*
@@ -12,22 +13,28 @@ private val elevationDigits = """\d+""".r
 private def parseElevation(raw: String): Option[Elevation] =
   elevationDigits.findFirstIn(raw).flatMap(_.toIntOption).map(Elevation(_))
 
+private def parseDetails(doc: Document): Map[String, String] =
+  doc.select("dl.product-detail dd").asScala.flatMap { dd =>
+    for
+      nameEl  <- Option(dd.selectFirst("meta[itemprop=name]"))
+      valueEl <- Option(dd.selectFirst("span[itemprop=value]"))
+    yield nameEl.attr("content") -> valueEl.text.trim
+  }.toMap
+
+private def parseBreadcrumbs(doc: Document): List[String] =
+  doc
+    .select("ol[itemtype$=BreadcrumbList] li span[itemprop=name]")
+    .asScala
+    .map(_.text.trim)
+    .toList
+
 def parseMeileafTea(html: String, url: URL): IO[ParseError, TeaInfo] =
   for
     doc   <- ZIO.attempt(Jsoup.parse(html)).orDie
     title <- parseElementText(doc, "h1.product-info__title")
     name  <- parseElementText(doc, "h2.product-info__subtitle")
-    details = doc.select("dl.product-detail dd").asScala.flatMap { dd =>
-      for
-        nameEl  <- Option(dd.selectFirst("meta[itemprop=name]"))
-        valueEl <- Option(dd.selectFirst("span[itemprop=value]"))
-      yield nameEl.attr("content") -> valueEl.text.trim
-    }.toMap
-    breadcrumbNames = doc
-      .select("ol[itemtype$=BreadcrumbList] li span[itemprop=name]")
-      .asScala
-      .map(_.text.trim)
-      .toList
+    details = parseDetails(doc)
+    breadcrumbNames = parseBreadcrumbs(doc)
     teaTypeName <- ZIO
       .fromOption(breadcrumbNames.lift(1))
       .orElseFail(ParseError("missing tea type in breadcrumbs"))
